2b62ac334dde3016c4320d8736c0ab9ebfadf913
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-11-09 12:13:47 -0500

    Add /products route to Routes.js and add /products link to Navbar.js

--- client/components/Products/AllProduct.js
+++ client/components/Products/AllProduct.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const AllProducts = props => {};
+const DUMMY_DATA = {
+  type: 'Guitar',
+  brand: 'Gibson',
+  model: 'Les Paul',
+  price: 100,
+  imageUrl: 'www.example.com',
+};
+
+const AllProducts = props => {
+  return <div>All Products</div>;
+};
 
 export default AllProducts;
