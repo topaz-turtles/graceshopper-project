02f4d9fb87b6638c6dfa8b6554106b888f04509e
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-11-16 11:50:11 -0500

    Refactor to declare cart with useSelector at top of component and update state on localStorage deletion

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   fetchItems,
   removeItemFromCart,
   editItemInCart,
-} from "../store/cart/cart";
-import { Link } from "react-router-dom";
+} from '../store/cart/cart';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const user = useSelector((state) => state.auth);
+  const user = useSelector(state => state.auth);
+  let cart = useSelector(state => state.cart);
+  const [cartState, setCartState] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -23,11 +25,13 @@ const Cart = () => {
       //convert local storage to array of objects from string
       let cartArr = JSON.parse(localStorage.product);
       //filter out items which are not the itemid
-      let items = cartArr.filter((instrument) => instrument.id !== itemId);
+      let items = cartArr.filter(instrument => instrument.id !== itemId);
+      //save cart state before converting to string
+      setCartState(items);
       //convert new array to string
       items = JSON.stringify(items);
       //set localStorage to new array without deleted items
-      localStorage.setItem("product", items);
+      localStorage.setItem('product', items);
 
       //else we are a user logged in and need to dispatch:
     } else {
@@ -39,20 +43,16 @@ const Cart = () => {
     dispatch(editItemInCart(userId, itemId, event.target.value));
   };
 
-  // cart is [] for now while we check if we are logged in or a guest
-  let cart = [];
   //if not user id, then get and parse items from LS, else cart is []
   if (!user.id) {
     cart = localStorage.product
-      ? JSON.parse(localStorage.getItem("product"))
+      ? JSON.parse(localStorage.getItem('product'))
       : [];
   }
-  //if user id then we are a user and need our logged in user cart
-  if (user.id) {
-    cart = useSelector((state) => state.cart);
-  }
 
-  const mappedCart = cart.map((item) => {
+  //if user id then we are a user and need our logged in user cart
+
+  const mappedCart = cart.map(item => {
     console.log(item);
     let currentPrice = ((item.price * item.quantity) / 100).toFixed(2);
     return (
@@ -61,7 +61,7 @@ const Cart = () => {
 
         <h3>{`${item.brand} ${item.itemType}`}</h3>
         <div className="cart-item-price">
-          Price:{" "}
+          Price:{' '}
           <b>
             ${currentPrice} (
             <input
@@ -72,9 +72,7 @@ const Cart = () => {
               defaultValue={item.quantity}
               min="0"
               step="1"
-              onChange={(event) =>
-                quantityChangeHandler(user.id, item.id, event)
-              }
+              onChange={event => quantityChangeHandler(user.id, item.id, event)}
             />
             x)
           </b>
@@ -105,7 +103,7 @@ const Cart = () => {
   return (
     <div className="cart-container">
       <h1>{`${
-        user.username ? user.username.toUpperCase() : "Guest"
+        user.username ? user.username.toUpperCase() : 'Guest'
       }'s Cart'`}</h1>
       {mappedCart}
       <div className="cart-checkout">
